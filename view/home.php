773a73ba28488ed70c72999f5f34278abbb523d8
Denis Dal Ronco <denis.dalronco@gmail.com>
2026-04-13 13:49:57 +0200

    Added a simple array to test home page, still work in progess.

--- view/home.php
+++ view/home.php
@@ -1,3 +1,7 @@
+<?php
+    $my_data = $data;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +10,11 @@
     <title>Document</title>
 </head>
 <body>
+    <hr>
     Do est do dolor sit officia. Consectetur consequat ut est dolor officia laborum laboris in qui ea laboris anim cupidatat. Cupidatat labore ut amet laboris excepteur dolor fugiat deserunt et. Est nisi nisi voluptate dolore tempor cillum pariatur. Aute sint et velit officia quis id exercitation exercitation sit culpa occaecat. Nulla fugiat excepteur quis eu. Exercitation incididunt nostrud reprehenderit sunt nisi.
+    <hr>
+    <div>
+        <?php print_r($my_data); ?>
+    </div>
 </body>
 </html>
